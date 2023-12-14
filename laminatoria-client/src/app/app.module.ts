69737d28ebad744df491fe1b415043c4d61ce4e9
LAPTOP-G1SCCE12\Dart <dmitrypermyakov1990@gmail.com>
2023-12-14 12:22:22 +0500

    feat: header layout
    Вёрстка header мобильной и десктоп версии. Верстка футера

--- laminatoria-client/src/app/app.module.ts
+++ laminatoria-client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
--- laminatoria-client/src/app/header/header.component.ts
+++ laminatoria-client/src/app/header/header.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+	public isOpened: boolean = false
 
+	public openMenu(): void {
+		this.isOpened = !this.isOpened
+	}
 }
