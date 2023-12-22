cbd326cc30a25bfb938080b0a82f77568feb2277
LAPTOP-G1SCCE12\Dart <dmitrypermyakov1990@gmail.com>
2023-12-22 17:48:55 +0500

    feat: orders mobile layout, filter for orders layout

--- laminatoria-client/src/app/orders/orders.component.ts
+++ laminatoria-client/src/app/orders/orders.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { FilterService } from '../services/filter.service'
 
 @Component({
-  selector: 'app-orders',
-  templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.scss']
+	selector: 'app-orders',
+	templateUrl: './orders.component.html',
+	styleUrls: ['./orders.component.scss'],
 })
 export class OrdersComponent {
+	public isOpen: boolean = false
 
+	constructor(public filterService: FilterService) {}
+
+	openFilter(): void {
+		this.isOpen = this.filterService.toggleFilter()
+	}
 }
